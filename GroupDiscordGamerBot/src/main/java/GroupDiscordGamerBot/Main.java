a93ff9ebcaadaff2b33d46b7231666679ce58e18
D-Blake <dblake.cccmc@gmail.com>
2019-08-22 22:37:45 -0600

    Updated to include Audio
    join/leave done

--- GroupDiscordGamerBot/src/main/java/GroupDiscordGamerBot/Main.java
+++ GroupDiscordGamerBot/src/main/java/GroupDiscordGamerBot/Main.java
@@ -1,9 +1,5 @@
 package GroupDiscordGamerBot;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,12 +7,20 @@ import java.nio.file.Paths;
 
 import javax.security.auth.login.LoginException;
 
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+
 import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.VoiceChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.managers.AudioManager;
 
 public class Main extends ListenerAdapter{
+	Guild g;
 
 	public static void main(String[] args) throws LoginException, IOException {
 		// TODO Auto-generated method stub
@@ -31,9 +35,15 @@ public class Main extends ListenerAdapter{
 
 	@Override
 	public void onMessageReceived(MessageReceivedEvent event) {
+		g = event.getGuild();
+		VoiceChannel myChannel = g.getVoiceChannelsByName("music", true).get(0);
+		AudioManager audioManager = g.getAudioManager();
+		AudioPlayerManager playerManager = new DefaultAudioPlayerManager();
+		AudioPlayer player = playerManager.createPlayer();
 		System.out.println("We received a message from " +
 					event.getAuthor().getName() + ": " +
 					event.getMessage().getContentDisplay());
+		
 		String cmd;
 		if(event.getMessage().getContentRaw().trim().split(" ").length>1) {
 			cmd = event.getMessage().getContentRaw().trim().split(" ")[0].toString();
@@ -47,6 +57,14 @@ public class Main extends ListenerAdapter{
 				break;
 			case "!echo":
 				event.getChannel().sendMessage(event.getMessage().getContentRaw().substring(cmd.length()+1)).queue();
+				break;
+			case "!join":
+				audioManager.openAudioConnection(myChannel);
+				audioManager.setSendingHandler(new AudioPlayerSendHandler(player));
+				audioManager.openAudioConnection(myChannel);
+				break;
+			case"!leave":
+				audioManager.closeAudioConnection();
 				break;
 		}
 	}
